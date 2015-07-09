578f1b3bb6aee752a9b816efdf00b49aa4768d3a
Jens <jdisselhoff@gmail.com>
2015-07-09 15:49:54 +0200

    splitting tags

--- app/controllers/api/websites_controller.rb
+++ app/controllers/api/websites_controller.rb
@@ -2,8 +2,14 @@ class Api::WebsitesController < ApplicationController
   protect_from_forgery :except => :create 
   def create
     @website = Website.first_or_create(url: wp[:url], description: wp[:description], user_email: wp[:user_email])    
-    @tag = Tag.first_or_create(name: wp[:name])
-    @website.tags << @tag
+    @tag_string = wp[:tags]
+    @tags = @tag_string.split(",")
+    @tags.each do |tag|
+      Tag.first_or_create(name: tag)
+      @website.tags << tag
+    end
+    
+    
     respond_to do |format|
       format.html { render json: @website.to_json }
       format.json { render json: @website.to_json }
