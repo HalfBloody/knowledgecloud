3a6d8640c6d1c4214ddf7377af56501da608a256
Jens <jdisselhoff@gmail.com>
2015-07-09 15:28:40 +0200

    fixed minor bug

--- app/controllers/api/websites_controller.rb
+++ app/controllers/api/websites_controller.rb
@@ -1,12 +1,12 @@
 class Api::WebsitesController < ApplicationController
   protect_from_forgery :except => :create 
   def create
-    @website = Website.update_or_create(url: wp[:url], description: wp[:description], user_email: wp[:user_email])    
-    @tag = Tag.update_or_create(name: wp[:name])
+    @website = Website.first_or_create(url: wp[:url], description: wp[:description], user_email: wp[:user_email])    
+    @tag = Tag.first_or_create(name: wp[:name])
     @website.tags << @tag
     respond_to do |format|
       format.html {}
-      format.json {}
+      format.json { render status: 200 }
     end
   end
 
