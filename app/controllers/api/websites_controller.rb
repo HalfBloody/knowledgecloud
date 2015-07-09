2bd363c25c9e13bd0fa20397f8a39ac0f4027bee
Jens <jdisselhoff@gmail.com>
2015-07-09 16:23:08 +0200

    fixing bug with tag model

--- app/controllers/api/websites_controller.rb
+++ app/controllers/api/websites_controller.rb
@@ -1,12 +1,13 @@
 class Api::WebsitesController < ApplicationController
-  # protect_from_forgery :except => :create 
+  protect_from_forgery :except => :create 
   def create
-    @website = Website.create_with(description: wp[:description], user_email: wp[:user_email]).first_or_create(url: wp[:url])
+
+    @website = Website.create(url: wp[:url], description: wp[:description], user_email: wp[:user_email])
     @tag_string = wp[:tags]
     @tags = @tag_string.split(",")
     @tags.each do |tag|
-      Tag.first_or_create(name: tag)
-      @website.tags << tag
+      @tag = Tag.find_or_create_by(name: tag)
+      @website.tags << @tag
     end
     
     
